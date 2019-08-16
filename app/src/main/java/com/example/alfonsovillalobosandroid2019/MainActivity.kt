6b6f94d9f49c293e885378c47bc5bae0b24eb3ff
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-08-15 19:26:09 -0600

    Ya se mando a otro layout

--- app/src/main/java/com/example/alfonsovillalobosandroid2019/MainActivity.kt
+++ app/src/main/java/com/example/alfonsovillalobosandroid2019/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.alfonsovillalobosandroid2019
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnHelloWorld.setOnClickListener {
+            val intentHelloWorld = Intent(this, HelloWorld::class.java)
+            startActivity(intentHelloWorld)
+        }
     }
 }
