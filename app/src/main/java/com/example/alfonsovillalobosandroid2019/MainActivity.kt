4685a050fb3b2d241b4270fa0c118c91949690eb
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-08-20 00:01:24 -0600

    Se limpio actividad principal

--- app/src/main/java/com/example/alfonsovillalobosandroid2019/MainActivity.kt
+++ app/src/main/java/com/example/alfonsovillalobosandroid2019/MainActivity.kt
@@ -12,9 +12,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setClickBtnHelloWorld()
+        setClickBtnTimeFighter()
+    }
+
+    /*
+    * Funcion encargada de inicializar
+    * el button de hello world*/
+    private fun setClickBtnHelloWorld(){
         btnHelloWorld.setOnClickListener {
             startActivity(Intent(this, HelloWorld::class.java))
         }
+    }
+
+    /*
+    * Funcion encargada de inicializar
+    * el button de setTimeFIghter*/
+    private fun setClickBtnTimeFighter(){
         btnTimeFighter.setOnClickListener {
             startActivity(Intent(this, TimeFighter::class.java))
         }
