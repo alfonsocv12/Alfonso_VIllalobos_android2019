7e4aa809ebd58a720e4175507c3ce2258af83943
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-08-19 23:55:05 -0600

    Se agrego botton de restart y se modifico un poco el codigo

--- app/src/main/java/com/example/alfonsovillalobosandroid2019/TimeFighter.kt
+++ app/src/main/java/com/example/alfonsovillalobosandroid2019/TimeFighter.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_time_fighter.*
 import java.nio.file.attribute.AclFileAttributeView
 
@@ -13,7 +14,8 @@ class TimeFighter : AppCompatActivity() {
     * Esta es la parte donde se
     * inician las variables*/
     var score = 0
-    var startTime:Long = 60000
+    var startTime:Long = 20000
+    var countDownInterval:Long = 1000
     lateinit var timer : CountDownTimer
 
     /*
@@ -22,7 +24,7 @@ class TimeFighter : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_fighter)
         gameScore1.text = getString(R.string.score, score.toString())
-        timeLeft1.text = getString(R.string.timeLeft, (startTime / 1000).toString())
+        timeLeft1.text = getString(R.string.time_left, (startTime / countDownInterval).toString())
         setTimer()
         btnTapMe.setOnClickListener {
             if(score == 0){
@@ -30,19 +32,32 @@ class TimeFighter : AppCompatActivity() {
             }
             incrementScore()
         }
+
+        btnRestart.setOnClickListener {
+            restartGame()
+        }
+    }
+
+    /*
+    * Funcion encargada
+    * de reiniciar el juego*/
+    private fun restartGame(){
+        score = 0
+        btnRestart.isVisible = false
+        btnTapMe.isEnabled = true
+        timer.cancel()
+        gameScore1.text = getString(R.string.score, score.toString())
+        timeLeft1.text = getString(R.string.time_left, (startTime / countDownInterval).toString())
     }
 
     /*
     * Funcion encargada de iniciar el
     * timer*/
     private fun setTimer(){
-        timer = object: CountDownTimer(startTime, 1000) {
-
+        timer = object: CountDownTimer(startTime, countDownInterval) {
             override fun onTick(millisUntilFinished: Long) {
-                val timeLeftSeconds = millisUntilFinished / 1000
-                timeLeft1.text = getString(R.string.timeLeft, timeLeftSeconds.toString())
+                timeLeft1.text = getString(R.string.time_left, (millisUntilFinished / countDownInterval).toString())
             }
-
             override fun onFinish() {
                 endGame()
             }
@@ -55,6 +70,7 @@ class TimeFighter : AppCompatActivity() {
     private fun endGame(){
         Toast.makeText(this, getString(R.string.end_game, score.toString()), Toast.LENGTH_SHORT).show()
         btnTapMe.isEnabled = false
+        btnRestart.isVisible = true
     }
 
     /*
