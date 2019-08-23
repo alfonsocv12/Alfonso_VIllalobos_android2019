fd180a19bbcf03241ae5414b6d94726b64f2ffb3
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-08-22 19:55:25 -0600

    Se agregan los xml

---
+++ app/src/main/java/com/example/alfonsovillalobosandroid2019/TempConverter.kt
@@ -0,0 +1,50 @@
+package com.example.alfonsovillalobosandroid2019
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_temp_converter.*
+
+class TempConverter : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temp_converter)
+        setClickbtnConvert()
+    }
+
+    /*
+    * Funcion encargada de inicializar el
+    * boton de conversion*/
+    private fun setClickbtnConvert(){
+        btnConvert.setOnClickListener {
+            convert()
+        }
+    }
+
+    /*
+    * Funcion encargada de convertir centigrados a
+    * farenheit*/
+    private fun convert(){
+        val gradesDouble = ((editTextTemp.text.toString().toDouble() - 32.0) * (5.0 / 9.0))
+        returnGradesCelcius(gradesDouble)
+        changeColor(gradesDouble)
+    }
+
+    /*
+    * Return convert of grades*/
+    private fun returnGradesCelcius(grades: Double){
+        Toast.makeText(this, grades.toString(), Toast.LENGTH_LONG).show()
+    }
+
+    /*
+    * Funcion encargada de cambiar
+    * el color de atras*/
+    private fun changeColor(grades: Double){
+        when (grades){
+            in 0.0..10.0 -> layoutTempConverter.setBackgroundColor(Color.rgb(12,12,12))
+            in 10.0..30.0 -> layoutTempConverter.setBackgroundColor(Color.rgb(14,20,5))
+        }
+    }
+}
