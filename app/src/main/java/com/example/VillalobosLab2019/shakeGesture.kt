7b4624a1d4fd1eb55dfe43c8cf176c87eed059cf
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-10-03 19:20:40 -0600

    Se continuo con rps

--- app/src/main/java/com/example/VillalobosLab2019/shakeGesture.kt
+++ app/src/main/java/com/example/VillalobosLab2019/shakeGesture.kt
@@ -5,20 +5,32 @@ import android.hardware.Sensor
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_shake_gesture.*
 
-class HelloWorld : AppCompatActivity() {
+class shakeGesture : AppCompatActivity() {
     private var mSensorManager: SensorManager? = null
     private var mSensorListener: ShakeEventListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hello_world)
+        setContentView(R.layout.activity_shake_gesture)
         mSensorManager = this.getSystemService(Context.SENSOR_SERVICE) as SensorManager?
         mSensorListener = ShakeEventListener()
-        
+
         this.mSensorListener!!.setOnShakeListener {
-            Toast.makeText(this, "Shake!!", Toast.LENGTH_LONG).show()
+            switch((1..3).shuffled().first()){
+                case 1:
+                    imgRPS.setImageResource(R.drawable.paper);
+                    break;
+                default:
+                    imgRPS.setImageResource(R.drawable.images);
+                    break;
+            }
+            imgRPS.setImageResource(R.drawable.paper)
+            imgRPS.setImageResource(R.drawable.rock)
+            imgRPS.setImageResource(R.drawable.scissor)
         }
     }
 
