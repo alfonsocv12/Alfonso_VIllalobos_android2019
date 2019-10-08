1e0f03ae6f2b6c5b9b2cfe8f21369ed2297944b9
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-10-07 18:43:49 -0600

    Se agrego scrol

---
+++ app/src/main/java/com/example/VillalobosLab2019/sharedPreferences.kt
@@ -0,0 +1,12 @@
+package com.example.VillalobosLab2019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sharedPreferences : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_preferences)
+    }
+}
